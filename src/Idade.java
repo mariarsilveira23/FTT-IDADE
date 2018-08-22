08ae5ab0371f95d81be88133dc317fdf9075ffc6
081160042 <081160042@HPFS-CE-LAB6930.cefsa.corp.local>
2018-08-21 22:19:53 -0300

    TESTE

---
+++ src/Idade.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Idade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Idade...");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Informe o ano de nascimento:");
+		
+		try {
+			System.out.println("Sua idade é: " + (2018 - scanner.nextInt()));
+		}
+		catch(Exception e)
+		{
+			System.err.println("Não foi possível calcular :(");
+			e.printStackTrace();
+		}
+				
+		scanner.close();
+
+	}
+
+}
